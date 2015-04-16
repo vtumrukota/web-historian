80e416abde7f453d901e2cd25d50639cc350e672
vtumrukota <vtumrukota@gmail.com>
2015-04-16 11:47:41 -0700

    Pass all basic requirements

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,5 +1,12 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
 var archive = require('../helpers/archive-helpers.js');
+var CronJob = require('cron').CronJob;
 
-archive.downloadUrls();
+var job = new CronJob({
+  cronTime: '00 * * * * *',
+  onTick: archive.downloadUrls,
+  start: true,
+  timeZone: 'America/Los_Angeles'
+});
+
